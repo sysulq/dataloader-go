d257947452ab0b42d83c2e70f4af65c6ccbeda0a
大可 <hnlq.sysu@gmail.com>
2024-07-31 11:14:26 +0800

    chore: improve example

--- example_test.go
+++ example_test.go
@@ -9,18 +9,16 @@ import (
 	"github.com/sysulq/dataloader-go"
 )
 
-func exampleLoader(ctx context.Context, keys []int) []dataloader.Result[string] {
-	results := make([]dataloader.Result[string], len(keys))
-
-	for i, key := range keys {
-		results[i] = dataloader.Wrap(fmt.Sprintf("Result for %d", key), nil)
-	}
-	return results
-}
-
 func TestExample(t *testing.T) {
 	loader := dataloader.New(
-		exampleLoader,
+		func(ctx context.Context, keys []int) []dataloader.Result[string] {
+			results := make([]dataloader.Result[string], len(keys))
+
+			for i, key := range keys {
+				results[i] = dataloader.Wrap(fmt.Sprintf("Result for %d", key), nil)
+			}
+			return results
+		},
 		dataloader.WithCache(100, time.Minute),
 		dataloader.WithBatchSize(50),
 		dataloader.WithWait(5*time.Millisecond),
@@ -31,36 +29,86 @@ func TestExample(t *testing.T) {
 	// Load
 	data, err := loader.Load(ctx, 1).Unwrap()
 	if err != nil {
-		fmt.Printf("Error: %v\n", data)
+		t.Errorf("Unexpected error: %v", err)
 	} else {
 		fmt.Printf("Result: %s\n", data)
+		// Output:
+		// Result: Result for 1
 	}
 
-	// AsyncLoad
+	// Go
 	ch := loader.Go(ctx, 2)
 	result := <-ch
-
-	fmt.Printf(result.Unwrap())
+	data, err = result.Unwrap()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	} else {
+		fmt.Printf("Result: %s\n", data)
+		// Output:
+		// Result: Result for 2
+	}
 
 	// LoadMany
 	results := loader.LoadMany(ctx, []int{3, 4, 5})
 	for _, result := range results {
 		data, err := result.Unwrap()
 		if err != nil {
-			fmt.Printf("Error: %v\n", err)
+			t.Errorf("Unexpected error: %v", err)
 		} else {
 			fmt.Printf("Result: %s\n", data)
+			// Output:
+			// Result: Result for 3
+			// Result: Result for 4
+			// Result: Result for 5
 		}
 	}
 
 	// LoadMap
-	resultsMap := loader.LoadMap(ctx, []int{6, 7, 8})
-	for _, result := range resultsMap {
-		data, err := result.Unwrap()
+	keys := []int{6, 7, 8}
+	resultsMap := loader.LoadMap(ctx, keys)
+	for _, key := range keys {
+		data, err := resultsMap[key].Unwrap()
 		if err != nil {
-			fmt.Printf("Error: %v\n", err)
+			t.Errorf("Unexpected error: %v", err)
 		} else {
 			fmt.Printf("Result: %s\n", data)
+			// Output:
+			// Result: Result for 6
+			// Result: Result for 7
+			// Result: Result for 8
 		}
+	}
+
+	// Prime
+	loader.Prime(ctx, 8, "Prime result")
+	data, err = loader.Load(ctx, 8).Unwrap()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	} else {
+		fmt.Printf("Result: %s\n", data)
+		// Output:
+		// Result: Prime result
+	}
+
+	// Clear
+	loader.Clear(7)
+	data, err = loader.Load(ctx, 7).Unwrap()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	} else {
+		fmt.Printf("Result: %s\n", data)
+		// Output:
+		// Result: Result for 7
+	}
+
+	// ClearAll
+	loader.ClearAll()
+	data, err = loader.Load(ctx, 8).Unwrap()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	} else {
+		fmt.Printf("Result: %s\n", data)
+		// Output:
+		// Result: Result for 8
 	}
 }
